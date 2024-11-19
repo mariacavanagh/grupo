bf53c1d0be02e9484032a9d8e4b5164a0ec51ea5
Maria <mcavanaghnicholson@udesa.edu.ar>
2024-11-19 16:32:51 -0300

    hice el formulario y un poco del fetch

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-fetch('https://dummyjson.com/recipes/1')
-.then(res => res.json())
-.then(console.log);
+fetch('https://dummyjson.com/recipes')
+    .then (function(response){
+        return response.json();
+
+    })
+    .then(function(data){
+        console.log(data);
+        let mprincipal = document.querySelector(".mprincipal")
+        let titulo = ""
+
+        for (let i=0; i<10; i++){
+            console.log(i)
+            titulo += `<>`
+
+        }
+        menu.innerHTML= titulo
+        
+
+    })
+    .catch(function(error){
+        console.log(error)
+    })
