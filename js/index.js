7e775f6a37fd70b65db432e675c8b64da36cb30e
Maria <mcavanaghnicholson@udesa.edu.ar>
2024-11-20 16:53:58 -0300

    pusimos todas las fotos, el logo, y el cargar mas, entre otras cosas

--- js/index.js
+++ js/index.js
@@ -10,10 +10,28 @@ fetch('https://dummyjson.com/recipes')
 
         for (let i=0; i<10; i++){
             console.log(i)
-            titulo += `<>`
+            titulo += `<article class= "fotos_art">
+            <img src=${data.recipes[i].image} alt= "" >
+            <article class="foto_plato"> <a class="plato" href="./index.html?id=${data.recipes[i].id}">
+            ${data.recipes[i].name}</a>
+            <p class= "dif"> Nivel de dificultad: ${data.recipes[i].difficulty}</p></article> </article>`;
 
         }
-        menu.innerHTML= titulo
+        mprincipal.innerHTML= titulo
+
+        let cargarmasfotos = document.querySelector('.boton_mas')
+        cargarmasfotos.addEventListener('click', function(){
+            for(let i=10; i<20; i++){
+                titulo += `<article class="foto_plato">
+                <img src=${data.recipes[i].image} alt="">
+                <div> <a class= "plato" href= "./index.html?id=${data.recipes[i].id}">
+                ${data.recipes[i].name} </a> 
+                <p class= "dif"> Nivel de dificultad: ${data.recipes[i].difficulty}</p>
+                </div>
+                </article>`;
+                mprincipal.innerHTML= titulo
+            }
+        })
         
 
     })
