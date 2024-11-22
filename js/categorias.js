6ae08a35dafb7afd0e25867e9bcb484f8dae1b7b
Teodelina Lacroze <teodelina@MacBook-Air-de-Teodelina.local>
2024-11-22 12:37:51 -0300

    js cat css result y cat

---
+++ js/categorias.js
@@ -0,0 +1,25 @@
+fetch('https://dummyjson.com/recipes/tags')
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+
+    let seccion = document.querySelector("#sectioncate");
+    let cat = "";
+
+    for (let i=0; i<data.length;i++){
+        cat+=
+        
+        `
+        <section class="articlecate">
+            <a href="./categorias.html" class="acate">${data[i]}</a>
+        </section>`
+    }
+
+    seccion.innerHTML = cat;
+})
+
+.catch(function(error){
+    console.log("Error ", error );
+});
